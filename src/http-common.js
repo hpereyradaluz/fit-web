dedf3d7a94ce8fc7fa7eb4b2feb5a370cc62da6e
Hernan <hpereyradaluz@gmail.com>
2020-10-11 11:02:33 -0300

    http common

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://secret-anchorage-07531.herokuapp.com/',
+  baseURL: 'https://secret-anchorage-07531.herokuapp.com/',
   headers: {
     'Content-type': 'application/json',
   },
