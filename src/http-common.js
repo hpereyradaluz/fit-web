c7a8e9974a97da6df6d0c3e09a744e71adf74af5
Hernan <hpereyradaluz@gmail.com>
2020-11-10 12:44:16 -0300

    coments

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://localhost:5000/',
+  baseURL: 'http://localhost:5000/', //baseUrl for conenct to backed api
   headers: {
     'Content-type': 'application/json',
   },
--- src/services/AccuWeatherService.js
+++ src/services/AccuWeatherService.js
@@ -2,19 +2,19 @@ import http from '../http-common'
 
 const getPositionSearch = (lat, lon) => {
   return http.get(
-    `http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=FKvNoHRcfR1zwcvOmicvAlMJiTCQYSG6&q=${lat}%2C%20${lon}&language=es-Es`
+    `http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=<putyourapikey>&q=${lat}%2C%20${lon}&language=es-Es`
   )
 }
 
 const getOneHrOfHourlyForecasts = (key) => {
   return http.get(
-    `http://dataservice.accuweather.com/forecasts/v1/hourly/1hour/${key}?apikey=FKvNoHRcfR1zwcvOmicvAlMJiTCQYSG6&language=es-ES`
+    `http://dataservice.accuweather.com/forecasts/v1/hourly/1hour/${key}?apikey=<putyourapikey>&language=es-ES`
   )
 }
 
 const getTwelveHsOfHourlyForecasts = (key) => {
   return http.get(
-    `http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/${key}?apikey=FKvNoHRcfR1zwcvOmicvAlMJiTCQYSG6&language=es-ES`
+    `http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/${key}?apikey=<putyourapikey>&language=es-ES`
   )
 }
 
